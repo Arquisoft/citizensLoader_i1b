0f53ff5a3fc4eec1e95d3a82a5d1e1fce517b256
Unknown <nfm_95@hotmail.com>
2017-02-09 12:58:03 +0100

    Añadida documentacion y pequeños cambios

--- src/main/java/es/uniovi/asw/persistence/InsertP.java
+++ src/main/java/es/uniovi/asw/persistence/InsertP.java
@@ -17,47 +17,41 @@ import es.uniovi.asw.password.*;
 public class InsertP implements Insert{
 
 	@Override
-	public List<Citizen> insert(List<CitizenInfo> citizenValues, String path) {
+	public List<Citizen> insert(List<CitizenInfo> citizenValues) {
 		// TODO Auto-generated method stub
 		List<Citizen> citizens = new ArrayList<Citizen>();
 		Citizen citizen;
 		// Inserta y verifica en la base de datos
 		for (CitizenInfo v : citizenValues) {
-		Date date= new Date();
+			//se procesa y transforma la fecha
+			Date date= new Date();
 			try{
 				DateFormat format= new SimpleDateFormat("dd/MM/yyyy");
 				date= format.parse(v.getBirthday());
 			}
 			catch (ParseException e) {
 				// TODO: handle exception
-			}
-			
-			
-				citizen = new Citizen(v.getFirstName(),v.getLastName(),date, v.getEmail(), v.getNIF(),v.getAddress(),v.getNationality(),
-						Integer.parseInt((v.getPollingStationCode().replace(".0", ""))));
-				PasswordGenerator.generatePasswords(citizen);
-				
-				try {
-					Parser.voterRepository.save(citizen);
-					citizens.add(citizen);
-				} catch (DataIntegrityViolationException e) {
-					citizen = Parser.voterRepository.findByEmail(citizen.getEmail());
-					citizens.add(citizen);
-				
+			}			
+			citizen = new Citizen(v.getFirstName(),v.getLastName(),date, v.getEmail(), v.getNIF(),v.getAddress(),v.getNationality(),
+					Integer.parseInt((v.getPollingStationCode().replace(".0", ""))));
+			PasswordGenerator.generatePasswords(citizen);
+
+			try {
+				Parser.voterRepository.save(citizen);
+				citizens.add(citizen);
+			} catch (DataIntegrityViolationException e) {
+				citizen = Parser.voterRepository.findByEmail(citizen.getEmail());
+				citizens.add(citizen);
+
 			}
 		}
-		
-		System.out.println(citizenValues.size() + " filas leídas del fichero");
-		
-		if (citizenValues.size() > citizens.size())
-			System.out.println((citizenValues.size() - citizens.size()) + " filas con errores en el fichero (ver fails.log para más información)");
-		
-		System.out.println("Se han registrado " + citizens.size() + " votantes y generado las cartas personales");
-		
-		// Devuelve los votantes insertados
+
+		System.out.println("Se han registrado " + citizens.size());
+
+		// Devuelve los votantes insertados 
 		return citizens;
 	}
 
-	
+
 
 }
