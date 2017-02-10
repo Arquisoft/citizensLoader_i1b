942f1a35a95986b85e010e7fa53aef7dea30c3a3
Muhlach <javiermuhlach@hotmail.com>
2017-02-10 14:58:51 +0100

    Changing password class

--- src/main/java/es/uniovi/asw/password/PasswordGenerator.java
+++ src/main/java/es/uniovi/asw/password/PasswordGenerator.java
@@ -1,12 +1,36 @@
 package es.uniovi.asw.password;
 
+import java.util.List;
+import java.util.Random;
+
 import es.uniovi.asw.model.Citizen;
 
 public class PasswordGenerator {
 
-	public static void generatePasswords(Citizen citizen) {
+	private static final String pasworsPossibilities = "ABCDEFGHIJKLMNOPQRSTUVXYZ123456789";
+
+	public static String generatePasswords() {
 		// TODO Auto-generated method stub
-		
+		String password = "";
+
+		Random randGenerator = new Random();
+
+		for (int i = 0; i < 9; i++) {
+
+			password += pasworsPossibilities.charAt(randGenerator.nextInt(pasworsPossibilities.length()));
+
+		}
+		return password;
+	}
+	public static void generatePasswords(List<Citizen> citizen) {
+
+		for (Citizen cit : citizen) {
+			((Citizen) citizen).setPassword(generatePasswords());
+		}
+	}
+	
+	public static void generatePasswords(Citizen citizen) {
+		citizen.setPassword(generatePasswords());
 	}
 
 }
