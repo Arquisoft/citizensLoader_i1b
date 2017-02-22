144286356270c9e2b55612036324e9f527d53934
Diego Perez Lopez <perez@diegoprzl.com>
2017-02-22 10:45:21 +0100

    Initial commons-cli support

--- src/main/java/es/uniovi/asw/parser/Parser.java
+++ src/main/java/es/uniovi/asw/parser/Parser.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 import javax.transaction.NotSupportedException;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import es.uniovi.asw.persistence.Insert;
@@ -15,27 +21,82 @@ public class Parser {
 	private final static String PDF_COMMAND="pdf";
 	private final static String TXT_COMMAND="txt";
 	private final static String DOCX_COMMAND="docx";
+	
 	public static CitizenRepository citizenRepository;
 	private static ReadCitizens reader;
-	private static LetterGen letterGen = new LetterGenPdf();
+	private static LetterGen letterGen;
 
 
 	//We pass here the inputs in the command line in order to generate different writeformats
 	@Autowired
 	public static void run(String[] args) {
-		System.out.println(args[0]);
-		String filePath = args[0];
+		
+		String filePath = null;
+		String letterFormat = null;
+		CommandLineParser parser = null;
+		CommandLine cmdLine = null;
+		
+		// CLI program options
+		Options options = new Options();
+		options.addOption("f", true, "Input file location");
+		options.addOption("l", true, "Letter generation format. pdf, txt, and docx.");
+		options.addOption("h", "help", false, "Display this help message.");
+		// Text
+		String helpExample = "citizensLoader.jar -f <arg> -l <arg>";
+
+		// Parsing the arguments and checking that they're valid
+		try {
+			parser = new DefaultParser();
+			cmdLine = parser.parse(options, args);
+			HelpFormatter formatter = new HelpFormatter();
+			
+			// If there is the help flag then show the help and return.
+			if (cmdLine.hasOption("h")) {
+				formatter.printHelp(helpExample, options);
+				return;
+			}
+			
+			// Check that the file has been specified.
+			if (cmdLine.hasOption("f")) {
+				filePath = cmdLine.getOptionValue("f").trim();
+			}
+			else {
+				System.out.println("You must specify a file.");
+				formatter.printHelp(helpExample, options);
+				return;
+			}
+			// Check that the letter format has been specified.
+			if (cmdLine.hasOption("l")) {
+				letterFormat = cmdLine.getOptionValue("l").trim();
+			}
+			else {
+				System.out.println("You must specify a letter generation format.");
+				formatter.printHelp(helpExample, options);
+				return;
+			}
+		} catch (ParseException e1) {
+			System.out.println("The parameters were incorrectly formatted.");
+			return;
+		}
+		
+		// We start executing the program.
+		System.out.println("File location: "+filePath);
+		System.out.println("Letter generation format: "+letterFormat);
+		
 		try {
 			createReader(filePath);
-			//the command line executing syntax is mode path
-			List<CitizenInfo> citizenInfo = reader.readCitizens(filePath); 
+			// the command line executing syntax is mode path
+			List<CitizenInfo> citizenInfo = reader.readCitizens(filePath);
 			Insert inserter = new InsertR();
 			List<Citizen> letCit = inserter.insert(citizenInfo);
 			// Generate the letters
-			generateLetter(letCit, args);
-		} catch (Exception e) {
+			generateLetter(letCit, letterFormat);
+		} catch (NotSupportedException e) {
+			System.out.println(e.getMessage());
+		}
+		catch (Exception e) {		
 			e.printStackTrace();
-		}		
+		}
 	}
 
 	/**
@@ -52,19 +113,20 @@ public class Parser {
 		}
 	}
 
-	private static void generateLetter(List<Citizen> letCit, String... input) throws IOException{
-		for(String i: input){
-			switch (i){
+	private static void generateLetter(List<Citizen> letCit, String letterFormat) throws IOException{
+			switch (letterFormat){
 			case PDF_COMMAND:
 				letterGen = new LetterGenPdf();
-				letterGen.generateLetters(letCit); 
+				letterGen.generateLetters(letCit);
+				break;
 			case TXT_COMMAND:
 				letterGen = new LetterGenTxt();
-				letterGen.generateLetters(letCit); 
+				letterGen.generateLetters(letCit);
+				break;
 			case DOCX_COMMAND:
 				letterGen = new LetterGenDocx();
-				letterGen.generateLetters(letCit); 
-			}
+				letterGen.generateLetters(letCit);
+				break;
 		}
 
 	}
